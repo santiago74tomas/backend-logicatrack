f3c44a1870cf11bee6742736bd57fe25664dbaeb
Santiagotomas74 <chrismar747@gmail.com>
2026-04-04 21:33:15 -0300

    fix cors

--- src/main/java/logitrack/config/CorsConfig.java
+++ src/main/java/logitrack/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5500") // frontend
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
